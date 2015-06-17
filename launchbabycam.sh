cd387d1fe2588a6b55b781885944a0605511d72c
Justin Carlson <jcarlson1985420@gmail.com>
2015-06-16 20:07:34 -0500

    Minor changes to startup script

--- launchbabycam.sh
+++ launchbabycam.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 cd /
-cd /home/pi/babycam/nodejswebsite
+cd /home/pi/raspberry-pi-baby-cam/nodejswebsite
 sleep 1
 sudo forever start app.js
 
 #sleep for 15 seconds to ensure NodeJS starts up properly
 sleep 15
 
-cd /home/pi/babycam
+cd /home/pi/raspberry-pi-baby-cam
 sudo python ./babycamrunner.py
 
 cd /
